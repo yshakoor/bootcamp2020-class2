b9f02c807727fc7b2e8ed42db5d98d50ac5d7b08
Yasser Shakoor <yasser.shakoor@velocix.com>
2020-06-02 19:29:08 +0100

    Make changes to App.js file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
 	  <br/>
-	  Update by Yasser Shakoor 
+	  Update by Yasser Shakoor 2nd time 
 	</p>
         <a
           className="App-link"
